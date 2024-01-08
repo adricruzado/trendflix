311aa7f4dafdf989c0fa20c68d4b1d823f455e19
Adri Cruzado <adricruzado96@gmail.com>
2024-01-08 17:02:17 +0100

    Add movie service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'f95d1551a82ba33ca21c4574cdc897b6',
 };
 
 /*
