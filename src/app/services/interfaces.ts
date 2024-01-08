57718505f1209a5d83048f58f3b99d677951029b
Adri Cruzado <adricruzado96@gmail.com>
2024-01-08 18:47:38 +0100

    Update movies interfaces

--- src/app/services/interfaces.ts
+++ src/app/services/interfaces.ts
@@ -1,23 +1,23 @@
 export interface ApiResult {
   page: number;
   results: MovieResult[];
-  totalPages: number;
-  totalResults: number;
+  total_pages: number;
+  total_results: number;
 }
 
 export interface MovieResult {
   adult: boolean;
-  backdropPath: string;
-  genreIds: number[];
+  backdrop_path: string;
+  genre_ids: number[];
   id: number;
-  originalLanguage: string;
-  originalTitle: string;
+  original_language: string;
+  original_title: string;
   overview: string;
   popularity: number;
-  posterPath: string;
-  releaseDate: string;
+  poster_path: string;
+  release_date: string;
   title: string;
   video: boolean;
-  voteAverage: number;
-  voteCount: number;
+  vote_average: number;
+  vote_count: number;
 }
